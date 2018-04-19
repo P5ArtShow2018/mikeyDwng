1aa31218279244e83d5b1e722578d230024b15e1
P5ArtShow2018 <38104389+P5ArtShow2018@users.noreply.github.com>
2018-04-19 07:21:37 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(1000,1000)
+  background (0,200,0)
+  frameRate(60)
+}
+
+function draw() {
+  //noStroke()
+  stroke(1,255)
+  fill(random(1,255),random(1,255),random(1,255))
+  //,random(1,50))
+  //line(mouseY,500,100,mouseY)
+  ellipse(mouseX,mouseY,mouseX,mouseY)
+  ellipse(mouseY,mouseX,mouseY,mouseX)
+  ellipse(mouseY,mouseX,mouseX,mouseY)
+  ellipse(mouseX,mouseY,mouseY,mouseX)
+  //rect(mouseX,mouseY,mouseX,mouseY)
+
+  
+}
+
+function mousePressed() {
+  background(0,200,0)
+}
